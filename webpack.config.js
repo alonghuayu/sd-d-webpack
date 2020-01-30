c090689985ed5330bcabd63cee0758eb903e5f9a
longhuayu123 <lhy1234567891011@163.com>
2020-01-30 23:18:52 +0800

    管理资源

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 
 module.exports = {
@@ -5,5 +7,22 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
