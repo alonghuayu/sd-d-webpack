18d8a7b58cc50f88a3b37e6be49fd67b57eb8a6c
longhuayu123 <lhy1234567891011@163.com>
2020-01-31 21:31:23 +0800

    output

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './tips.svg';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
@@ -13,6 +15,11 @@ function component() {
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+    
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
