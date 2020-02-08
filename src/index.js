613576608d5655f4a59f06296872b09fbc1782c0
longhuayu123 <lhy1234567891011@163.com>
2020-02-09 00:02:21 +0800

    lazyloading buttom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 
-// import _ from 'lodash';
+import _ from 'lodash';
 import { cube, square } from './math.js';
 import './style.css';
 import Icon from './tips.svg';
 import printMe from './print.js';
 
-async function getComponent() {
+function component() {
+    var element = document.createElement('div');
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+    button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+        var print = module.default;
+
+        print();
+    });
+
+    return element;
+}
+document.body.appendChild(component());
+// async function getComponent() {
     // function getComponent() {
     // return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
     //     var element = document.createElement('div');
@@ -16,17 +34,17 @@ async function getComponent() {
 
     // }).catch(error => 'An error occurred while loading the component');
 
-    var element = document.createElement('div');
-    const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+//     var element = document.createElement('div');
+//     const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-}
+//     return element;
+// }
 
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+// getComponent().then(component => {
+//     document.body.appendChild(component);
+// })
 // function component() {
 //     // var element = document.createElement('div');
 //     var element = document.createElement('pre');
@@ -61,13 +79,13 @@ getComponent().then(component => {
 // let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 // document.body.appendChild(element);
 
-if (module.hot) {
-    module.hot.accept('./print.js', function () {
-        console.log('Accepting the updated printMe module!');
-        // printMe();
+// if (module.hot) {
+//     module.hot.accept('./print.js', function () {
+//         console.log('Accepting the updated printMe module!');
+//         // printMe();
 
-        document.body.removeChild(element);
-        element = component(); // 重新渲染页面后，component 更新 click 事件处理
-        document.body.appendChild(element);
-    })
-}
+//         document.body.removeChild(element);
+//         element = component(); // 重新渲染页面后，component 更新 click 事件处理
+//         document.body.appendChild(element);
+//     })
+// }
